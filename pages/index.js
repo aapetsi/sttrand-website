ed160b0a50b3603ef096ba8b0bc7d96d988e8cc9
Apetsi Ampiah <apetsiampiah@gmail.com>
2022-08-21 19:03:09 +0000

    wip

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,26 @@ import products from '../images/Screen Shot 2022-07-13 at 11.02.15 PM@2x.png'
 export default function Home() {
   return (
     <div>
+      <div>
+        <h1 className='text-center tracking-wide text-3xl font-black mb-6'>
+          A Platform Where Consumers
+          <br /> can Utilize Social Apps to buy Items
+          <br /> at a Discount
+        </h1>
+        <p className='text-center tracking-wide text-xl mb-10'>
+          Consumers have the option to purchase everyday household items
+          individually at
+          <br /> a standard retail price or they can partner with a
+          family/firend or other
+          <br /> consumers on our platform to get a discounted price.
+        </p>
+        <div className='text-center mb-10'>
+          <button className='w-[200px] h-[60px] bg-deep-blue text-white rounded-lg'>
+            Get the App
+          </button>
+        </div>
+      </div>
+
       <div className='bg-deep-blue'>
         <h1 className='text-center text-white pt-10 mb-6 text-4xl tracking-wider'>
           Become a Merchant
