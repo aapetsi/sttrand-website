a6a094124a15a0116160f5d1e052cb76b4b9d7d5
Apetsi Ampiah <apetsiampiah@gmail.com>
2022-10-15 00:30:33 +0000

    feat: add sttrand logo to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import SttrandLogo from './SttrandLogo'
 
 const Navbar = () => {
   return (
     <nav className='flex items-center justify-between flex-wrap p-6'>
-      <div className='flex w-1/2 items-center flex-shrink-0 text-white mr-72'>
-        <svg
-          className='bg-black-100 h-8 w-8 mr-2'
-          width='54'
-          height='54'
-          viewBox='0 0 54 54'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path d='M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z' />
-        </svg>
-        <span className='font-semibold text-xl tracking-tight text-black'>
-          Tailwind CSS
-        </span>
+      <div className='flex w-3/12 items-center flex-shrink-0 text-white mr-72'>
+        <SttrandLogo />
       </div>
 
       <div className='block lg:hidden'>
