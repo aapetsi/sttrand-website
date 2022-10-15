f18280fcffa906bbc81717ad047948acb601eef1
Apetsi Ampiah <apetsiampiah@gmail.com>
2022-10-15 00:37:34 +0000

    chore: remove hamburger icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,21 +6,8 @@ import SttrandLogo from './SttrandLogo'
 const Navbar = () => {
   return (
     <nav className='flex items-center justify-between flex-wrap p-6'>
-      <div className='flex w-3/12 items-center flex-shrink-0 text-white mr-72'>
+      <div className='flex w-3/12'>
         <SttrandLogo />
-      </div>
-
-      <div className='block lg:hidden'>
-        <button className='flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white'>
-          <svg
-            className='fill-current h-3 w-3'
-            viewBox='0 0 20 20'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <title>Menu</title>
-            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
-          </svg>
-        </button>
       </div>
 
       <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
