1738d44a03c332863e6e34db7e1144574ee48854
Apetsi Ampiah <apetsiampiah@gmail.com>
2022-10-20 06:58:44 +0000

    fix: window reference error

--- hooks/useWindowDimensions.jsx
+++ hooks/useWindowDimensions.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react'
 
-const getWindowDimensions = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
+
 
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState(0,0);
 
   useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
+    }
+
+    function getWindowDimensions() {
+      const { innerWidth: width, innerHeight: height } = window;
+      return {
+        width,
+        height
+      };
     }
 
     window.addEventListener('resize', handleResize);
